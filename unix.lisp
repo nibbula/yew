7d36e01a3290c4d8f321ae28d501744e31e1a374
Nibby Nebbulous <nibbula@gmail.com>
2016-12-20 13:42:01 -0800

    handle command names more robustly

--- unix.lisp
+++ unix.lisp
@@ -4322,27 +4322,34 @@ Possible values of STATUS and VALUE are:
 		  :args nil)))))
 	(foreign-free proc-list))))
   #+linux
-  (loop :with line :and pid
-     :for p :in (read-directory :dir "/proc/")
-     :when (every #'digit-char-p p)
-     :collect
-     (with-open-file (stm (s+ "/proc/" p "/stat"))
-       (setf line (split-sequence #\space (read-line stm))
-	     pid (parse-integer p))
-       (make-os-process
-	:id pid
-	:parent-id (parse-integer (elt line 3))
-	:group-id (parse-integer (elt line 4))
-	:terminal (parse-integer (elt line 6))
-	:text-size (parse-integer (elt line 22))
-	:resident-size (parse-integer (elt line 23))
-	:percent-cpu nil
-	:nice-level (parse-integer (elt line 18))
-	:usage nil
-	;; :command (elt line 1)
-	;; :args (get-process-command-line )))))
-	:command (subseq (elt line 1) 1 (1- (length (elt line 1))))
-	:args (get-process-command-line pid)))))
+  (let (line pid raw-line open-pos close-pos cmd uid)
+    (labels ((pos (p)
+	       "Adusted element in stat line so we can use documented indices."
+	       (elt line (- p 2))))
+      (loop :for p :in (read-directory :dir "/proc/")
+	 :when (every #'digit-char-p p)
+	 :collect
+	 (with-open-file (stm (s+ "/proc/" p "/stat"))
+	   (setf raw-line (read-line stm)
+		 open-pos (position #\( raw-line)
+		 close-pos (position #\) raw-line :from-end t)
+		 line (split-sequence #\space (subseq raw-line (+ 2 close-pos)))
+		 cmd (subseq raw-line (position #\( raw-line) close-pos)
+		 pid (parse-integer p)
+		 uid (file-status-uid (stat (s+ "/proc/" p))))
+	   (make-os-process
+	    :id pid
+	    :parent-id (parse-integer (pos 3))
+	    :group-id (parse-integer (pos 4))
+	    :user-id uid
+	    :terminal (parse-integer (pos 6))
+	    :text-size (parse-integer (pos 22))
+	    :resident-size (parse-integer (pos 23))
+	    :percent-cpu 0
+	    :nice-level (parse-integer (pos 18))
+	    :usage nil
+	    :command (subseq raw-line (1+ open-pos) close-pos)
+	    :args (get-process-command-line pid)))))))
 
 (defun suspend-process (&optional id)
   "Suspend the process with the given ID. If ID is NIL or not given, suspend
