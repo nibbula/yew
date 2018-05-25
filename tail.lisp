f55ff139b2bc273aee623fbff9bfd90f352230f4
Nibby Nebbulous <nibbula@gmail.com>
2018-05-25 10:51:17 -0700

    at least -f working a bit

--- tail.lisp
+++ tail.lisp
@@ -11,7 +11,7 @@
    ))
 (in-package :tail)
 
-(defun tail-forever (stream)
+(defun tail-forever (stream interval)
   (if (interactive-stream-p stream)
       (loop :with line
 	 :do
@@ -21,7 +21,7 @@
 	       (when line
 		 (write-line line *standard-output*)))
 	     (progn
-	       (sleep 1)
+	       (sleep interval)
 	       (setf line t)))
 	 :while line))
       (loop :with line
@@ -29,12 +29,22 @@
 	 (if (setf line (read-line stream nil nil))
 	     (write-line line *standard-output*)
 	     (progn
-	       (sleep 1)))))
+	       (sleep interval)))))
+
+(defun really-seekable (stream)
+  "Return true if STREAM is really seekable."
+  (and (file-position stream 0)
+       (handler-case
+	   (file-length stream)
+	 (type-error (c)		; according to the standard
+	   (declare (ignore c))
+	   nil))))
 
 (defun tail-lines (file-or-stream count forever)
-  "Output the last COUNT lines of FILE-OR-STREAM."
+  "Output the last COUNT lines of FILE-OR-STREAM. If FOREVER is true, use it as
+the time to sleep between checking for output."
   (with-open-file-or-stream (stream file-or-stream)
-    (let* ((seekable (file-position stream 0))
+    (let* ((seekable (really-seekable stream))
 	   (buf-len (nos:memory-page-size))
 	   (buf (make-string buf-len))
 	   (line-buf (make-stretchy-string 80))
@@ -44,7 +54,7 @@
 	   look-pos
 	   end)
       (if seekable
-	  (progn
+	  (progn ;;; @@@ This part is broken:
 	    (setf end (file-length stream)
 		  read-pos (max (- end buf-len) 0))
 	    (loop :while (and (file-position stream read-pos)
@@ -71,10 +81,27 @@
 	    (mapcar #'write-line lines))
 	  ;; Non-seekable streams
 	  (when (not (interactive-stream-p stream))
-	    (progn
-	      (error "I didn't do non-seekable streams yet."))))
+	    ;; Don't bother on interactive streams.
+	    (let ((lines (make-array count)) (i 0))
+	      (flet ((add-line (l)
+		       "Add L to the ring buffer LINES."
+		       (setf (aref lines (mod i count)) l)
+		       (incf i)))
+		;; Read until EOF.
+		(loop :with l
+		   :while (setf l (read-line stream nil))
+		   :do (add-line l))
+		;; Output lines from the ring buffer.
+		(loop :with limit = (min count i)
+		   :with j = (mod (- i limit) count)
+		   :and output = 0
+		   :while (< output limit)
+		   :do
+		   (write-line (aref lines (mod j count)))
+		   (incf output)
+		   (incf j))))))
       (when forever
-	(tail-forever stream))
+	(tail-forever stream forever))
       #| (finish-output *standard-output*) |#
       )))
 
@@ -88,6 +115,8 @@
    ;;  :help "The number of units to show.")
    (forever boolean :short-arg #\f
     :help "True to keep displaying lines added to the end of the file.")
+   (sleep-interval number :short-arg #\s :default 1
+    :help "Interval in seconds to sleep between checking for output when forever is true.")
    (files pathname :repeating t
     :help "Files to use as input."))
   "Output the last portion of input."
@@ -99,16 +128,18 @@
 	    (snip-bytes *standard-input* byte-count :before))
 	(when forever
 	  (with-open-file-or-stream (stream (car (last files)))
-	    (let ((seekable (file-position stream 0)))
+	    (let ((seekable (really-seekable stream)))
 	      (when (not seekable)
-		(error "I didn't do non-seekable streams yet."))
+		(error "I didn't implement non-seekable streams yet."))
 	      (file-position stream (file-length stream))
-	      (tail-forever stream)))))
+	      (tail-forever stream sleep-interval)))))
       (if files
 	  (loop :with i = 0 :and len = (length files)
 	     :for f :in files :do
-	     (tail-lines f line-count (and forever (= i (1- len))))
+	     (tail-lines f line-count
+			 (and forever (= i (1- len)) sleep-interval))
 	     (incf i))
-	  (tail-lines *standard-input* line-count forever))))
+	  (tail-lines *standard-input* line-count
+		      (and forever sleep-interval)))))
 
 ;; EOF
