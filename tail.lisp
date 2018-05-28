e5223e675240db28cabb9e2d4c4f898c023cde0f
Nibby Nebbulous <nibbula@gmail.com>
2018-05-28 16:24:39 -0700

    maybe it really works now?

--- tail.lisp
+++ tail.lisp
@@ -40,70 +40,64 @@
 	   (declare (ignore c))
 	   nil))))
 
-(defun tail-lines (file-or-stream count forever)
+(defun tail-lines (file-or-stream count &key forever)
   "Output the last COUNT lines of FILE-OR-STREAM. If FOREVER is true, use it as
 the time to sleep between checking for output."
+  (cond
+    ((zerop count) (return-from tail-lines nil))
+    ((minusp count)
+     ;; Assume a negtaive count is actually positive.
+     (setf count (- count))))
   (with-open-file-or-stream (stream file-or-stream)
     (let* ((seekable (really-seekable stream))
 	   (buf-len (nos:memory-page-size))
-	   (buf (make-string buf-len))
-	   (line-buf (make-stretchy-string 80))
-	   lines
-	   (line-count 0)
 	   read-pos
-	   look-pos
-	   end)
-      (if seekable
-	  (progn ;;; @@@ This part is broken:
-	    (setf end (file-length stream)
-		  read-pos (max (- end buf-len) 0))
-	    (loop :while (and (file-position stream read-pos)
-			      (>= read-pos 0))
-	       :do
-	       (read-sequence buf stream)
-	       (setf look-pos (1- buf-len))
-	       (loop :while (and (>= look-pos 0)
-				 (<= line-count count))
-		  :do
-		  (if (char= (aref buf look-pos) #\newline)
-		      (progn
-			(when (not (zerop line-count))
-			  (push (reverse line-buf) lines))
-			(stretchy-truncate line-buf)
-			(incf line-count))
-		      (stretchy-append line-buf (aref buf look-pos)))
-		  (decf look-pos))
-	       (when (>= line-count count)
-		 (return))
-	       (decf read-pos buf-len)
-	       (when (< read-pos 0)
-		 (setf read-pos 0)))
-	    (mapcar #'write-line lines))
-	  ;; Non-seekable streams
-	  (when (not (interactive-stream-p stream))
-	    ;; Don't bother on interactive streams.
-	    (let ((lines (make-array count)) (i 0))
-	      (flet ((add-line (l)
-		       "Add L to the ring buffer LINES."
-		       (setf (aref lines (mod i count)) l)
-		       (incf i)))
-		;; Read until EOF.
-		(loop :with l
-		   :while (setf l (read-line stream nil))
-		   :do (add-line l))
-		;; Output lines from the ring buffer.
-		(loop :with limit = (min count i)
-		   :with j = (mod (- i limit) count)
-		   :and output = 0
-		   :while (< output limit)
-		   :do
-		   (write-line (aref lines (mod j count)))
-		   (incf output)
-		   (incf j))))))
+	   (lines (make-array count))
+	   (i 0)) ;; line count & add position in lines
+      (labels ((add-line (l)
+		 "Add L to the ring buffer LINES."
+		 (setf (aref lines (mod i count)) l)
+		 (incf i))
+	       (read-lines ()
+		 "Just read everything until EOF"
+		 (loop :with l
+		    :while (setf l (read-line stream nil))
+		    :do (add-line l))))
+	;; Don't bother on non-seekable interactive streams.
+	(when (or seekable (not (interactive-stream-p stream)))
+	  ;; Read until EOF.
+	  (if seekable
+	      ;; Keep backing up until we read enough lines
+	      (loop :with inc = 1
+		 :do
+		 (setf read-pos (max 0 (- (file-length stream)
+					  (* buf-len inc))))
+		 ;; (format t "read-pos = ~s~%" read-pos)
+		 (file-position stream read-pos)
+		 (setf i 0)
+		 (read-lines)
+		 (setf inc (* inc 2))
+		 :while (and (> read-pos 0) (< i count)))
+	      ;; Non-seekable, so just read all lines.
+	      (read-lines))
+	  ;; Output lines from the ring buffer.
+	  (loop :with limit = (min count i)
+	     :with j = (mod (- i limit) count)
+	     :and output = 0
+	     :while (< output limit)
+	     :do
+	     (write-line (aref lines (mod j count)))
+	     (incf output)
+	     (incf j))))
+      (finish-output)
       (when forever
 	(tail-forever stream forever))
       #| (finish-output *standard-output*) |#
       )))
+
+;; @@@ I want to be able to support args like "-12" meaning "-n 12".
+;; I could hack it in to the arg processing here, like with :keys-as, but I want
+;; the automatic arg parsing to be able to support it.
 
 #+lish
 (lish:defcommand tail
@@ -117,6 +111,7 @@ the time to sleep between checking for output."
     :help "True to keep displaying lines added to the end of the file.")
    (sleep-interval number :short-arg #\s :default 1
     :help "Interval in seconds to sleep between checking for output when forever is true.")
+   ;; (list boolean :short-arg #\l :help "True to return lines as a list.")
    (files pathname :repeating t
     :help "Files to use as input."))
   "Output the last portion of input."
@@ -137,9 +132,9 @@ the time to sleep between checking for output."
 	  (loop :with i = 0 :and len = (length files)
 	     :for f :in files :do
 	     (tail-lines f line-count
-			 (and forever (= i (1- len)) sleep-interval))
+			 :forever (and forever (= i (1- len)) sleep-interval))
 	     (incf i))
 	  (tail-lines *standard-input* line-count
-		      (and forever sleep-interval)))))
+		      :forever (and forever sleep-interval)))))
 
 ;; EOF
