cd38d80712bd759514793fd4eb2518a3521736e1
Nibby Nebbulous <nibbula@gmail.com>
2018-06-19 04:38:01 -0700

    dashed lines if a terminal doesn't have underline

--- terminal-table.lisp
+++ terminal-table.lisp
@@ -21,37 +21,41 @@
   (declare (ignore table))
   (with-accessors ((separator text-table-renderer-separator)
 		   (stream text-table-renderer-stream)) renderer
-    (loop :with str
-       :and fmt = "~va"
-       :and len = (length titles)
-       :and size :and just
-       :for col :in titles
-       :and i :from 0 :below (length sizes)
-       :do
-       (setf size (car (aref sizes i))
-	     just (cadr (aref sizes i)))
-       (if (listp col)
-	   (setf str (first col)
-		 fmt (if (eql just :right) "~v@a" "~va"))
-	   (setf str col
-		 fmt "~va"))
-       (terminal-underline stream t)
-       (terminal-format stream fmt size
-	       (subseq (string-capitalize (substitute #\space #\_ str))
-		       0 (min (length str) size)))
-       (terminal-underline stream nil)
-       (when (< i (1- len))
-	 (terminal-write-string stream separator)))
-    (terminal-write-char stream #\newline)
+    (let ((has-underline (terminal-has-attribute stream :underline)))
+      (loop :with str
+	 :and fmt = "~va"
+	 :and len = (length titles)
+	 :and size :and just
+	 :for col :in titles
+	 :and i :from 0 :below (length sizes)
+	 :do
+	 (setf size (car (aref sizes i))
+	       just (cadr (aref sizes i)))
+	 (if (listp col)
+	     (setf str (first col)
+		   fmt (if (eql just :right) "~v@a" "~va"))
+	     (setf str col
+		   fmt "~va"))
+	 (when has-underline
+	   (terminal-underline stream t))
+	 (terminal-format stream fmt size
+			  (subseq (string-capitalize
+				   (substitute #\space #\_ str))
+				  0 (min (length str) size)))
+	 (when has-underline
+	   (terminal-underline stream nil))
+	 (when (< i (1- len))
+	   (terminal-write-string stream separator)))
+      (terminal-write-char stream #\newline)
 
-    ;; Lines
-    ;; (loop :with len = (length sizes)
-    ;;    :for i :from 0 :below len
-    ;;    :do
-    ;;    (tt-format "~v,,,va" (car (aref sizes i)) #\- #\-)
-    ;;    (when (< i (1- len))
-    ;; 	 (tt-write-string separator)))
-    ;; (tt-write-char #\newline)
-    ))
+      ;; Lines
+      (when (not has-underline)
+	(loop :with len = (length sizes)
+	   :for i :from 0 :below len
+	   :do
+	   (terminal-format stream "~v,,,va" (car (aref sizes i)) #\- #\-)
+	   (when (< i (1- len))
+	     (terminal-write-string stream separator)))
+	(terminal-write-char stream #\newline)))))
 
 ;; EOF
