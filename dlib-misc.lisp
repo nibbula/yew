97951780fac00ab5ccd434db47a9d932fe9d5634
Nibby Nebbulous <nibbula@gmail.com>
2018-08-24 17:35:11 -0700

    don't try to close a NIL stream

--- dlib-misc.lisp
+++ dlib-misc.lisp
@@ -1455,7 +1455,7 @@ defaults to character) with the contents of FILE-OR-STREAM."
 			    (when (> pos 0)
 			      (write-sequence buffer str :end pos))
 			    :while (= pos len)))))))
-      (when close-me
+      (when (and close-me stream)
 	(close stream)))
     result))
 
