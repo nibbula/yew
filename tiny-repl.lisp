5803d371b343ff6699d5f6dfd8017848c36dae26
Nibby Nebbulous <nibbula@gmail.com>
2018-10-29 21:22:51 -0700

    big cleanup and better error handling

--- tiny-repl.lisp
+++ tiny-repl.lisp
@@ -3,10 +3,10 @@
 ;;;
 
 ;;; TODO:
-;;;   - pasteability?
+;;;   - mutli-line
 ;;;     - make multi-line statements 1 history entry
-;;;     - some way to turn off completion (tab & ?) for pasting
-;;;       - but maybe bracketed paste mode is sufficient?
+;;;     - make enter just go to the next line in RL
+;;;       - Meta-M for really accept-line
 
 (declaim (optimize (speed 0) (safety 3) (debug 3) (space 0)
 		   (compilation-speed 0)))
@@ -22,12 +22,12 @@
   ))
 (in-package "TINY-REPL")
 
-(defparameter *real-eof-symbol* :Z-REAL-EOF)
-(defparameter *continue-symbol* :Z-CONTINUE)
-(defparameter *empty-symbol* :Z-EMPTY)
-(defparameter *error-symbol* :Z-ERROR)
-(defparameter *quit-symbol* :Z-QUIT)
-(defparameter *exit-symbol* :Z-EXIT)
+(defparameter *real-eof-symbol* 'repl-real-eof)
+(defparameter *continue-symbol* 'repl-continue)
+(defparameter *empty-symbol* 'repl-empty)
+(defparameter *error-symbol* 'repl-error)
+(defparameter *quit-symbol* 'repl-quit)
+(defparameter *exit-symbol* 'repl-exit)
 
 (defvar *repl-level* -1
   "How many recursive command loops are active.")
@@ -50,7 +50,7 @@
 	       #-ccl *lisp-implementation-nickname*
 	       pkg
 	       (> *repl-level* 0) *repl-level*
-	       (if prompt-supplied p *default-prompt*))))
+	       (if (and prompt-supplied p) p *default-prompt*))))
     ;; (rl::editor-write-string
     ;;  e (format nil "~a~@[:~a~]~:[~*~;:~d~]~a"
     ;; 	       #+(and ccl 32-bit-target) "CCL-32"
@@ -68,6 +68,7 @@
                   copy of this REPL-STATE.
   prompt-func     A propmpt function for RL.
   more		  If non-nil, a string of more input.
+  terminal        A terminal to use.
   terminal-name   Name of a terminal device or nil.
   terminal-class  A terminal class symbol.
   keymap          Custom keymap for RL or nil.
@@ -81,6 +82,7 @@
   prompt-func
   prompt-string
   more
+  terminal
   terminal-name
   terminal-class
   keymap
@@ -97,8 +99,8 @@
  Second value is true if we got an EOF."
   (with-slots (more) state
     (multiple-value-bind (val pos)
-	(ignore-errors (read-from-string more nil *continue-symbol*))
-      (if (or (eql val *continue-symbol*)
+	(ignore-errors (read-from-string more nil 'repl-continue))
+      (if (or (eql val 'repl-continue)
 	      (not (and more (stringp more))))
 	(progn
 	  (setf more nil)
@@ -185,110 +187,68 @@ The REPL also has a few commands:
 
 (defun repl-read (state)
   (with-slots (editor debug prompt-func prompt-string got-error more
-	       terminal-name terminal-class keymap) state
+	       terminal terminal-name terminal-class keymap) state
     (let ((result nil)
 	  (pre-str nil)
 	  (str nil))
-      (loop :while (eq *continue-symbol* (setf result
-        (handler-case
-         (handler-bind
-	     ((end-of-file #'(lambda (c)
-			       (declare (ignore c))
-			       (dbugf :repl "GOT read EOF - Continuing~%")
-			       (signal (make-condition 'repl-read-continue))))
- 	      ;;(condition #'(lambda (c)
- 	      ;;  (dbugf :repl "GOT something else - debug or signal~%")
-	      ;; ;; ??? Why can't we just use "debug" here?
-	      ;;  (if (repl-state-debug state)
-	      ;;    (invoke-debugger c)
-	      ;;    (signal c))))
-	      )
-	   (progn
-	    (dbugf :repl "editor before = ~a~%" editor)
-	    (if more
-		(progn
-		  (setf str more)
-		  (setf more nil)
-		  (dbugf :repl "Using MORE!~%"))
-		(if pre-str
-		    (setf (values str editor)
-			  (rl :eof-value *real-eof-symbol*
-			      :quit-value *quit-symbol*
-			      :editor editor
-			      :keymap keymap
-			      :terminal-name terminal-name
-			      :terminal-class terminal-class
-			      :context :repl
-			      :prompt ""))
-		    (progn
-		      (tt-finish-output)
-		      (finish-output)
-		      ;; (try-to-reset-curses)
-		      (setf (values str editor)
-			    (if prompt-string
-				(rl :eof-value *real-eof-symbol*
-				    :quit-value *quit-symbol*
-				    :editor editor
-				    :keymap keymap
-				    :terminal-name terminal-name
-				    :terminal-class terminal-class
-				    :context :repl
-				    :prompt prompt-string)
-				(rl :eof-value *real-eof-symbol*
-				    :quit-value *quit-symbol*
-				    :editor editor
-				    :terminal-name terminal-name
-				    :terminal-class terminal-class
-				    :keymap keymap
-				    :context :repl
-				    :output-prompt-func
-				    (if prompt-func
-					prompt-func
-					#'repl-output-prompt)))))))
-	    (dbugf :repl "str = ~s~%editor after = ~a~%" str editor)
-	    (cond
-	      ((and (stringp str) (equal 0 (length str)))
-	       *empty-symbol*)
-	      ((and (stringp str) (equal "." str))
-	       (dbugf :repl "Got a dot!~%")
-	       *exit-symbol*)
-	      ((equal str *real-eof-symbol*)
-	       (dbugf :repl "You got a *real-eof-symbol* !~%")
-	       *real-eof-symbol*)
-	      ((equal str *quit-symbol*)
-	       *quit-symbol*)
-;	      ((eq form *continue-symbol*))
-	      (t
-	       (dbugf :repl "Before read: pre-str = ~w str = ~w~%" pre-str str)
-	       (let ((cat (if pre-str
-			      (format nil "~a~%~a" pre-str str)
-			      str)))
-		 (multiple-value-bind (obj pos)
-		     (read-from-string cat nil *continue-symbol*)
-		   ;; Make MORE be the rest of the string, if any.
-		   (setf more
-			 (if (and (not (eql obj *continue-symbol*))
-				  (< pos (length cat)))
-			     (subseq cat pos)
-			     nil))
-		   obj))))))
-	  (repl-read-continue () *continue-symbol*)
-;	  (condition (c)
-	  ;; @@@ This hides where errors come from :(
-	  ;; (error (c)
-	  ;;   (setf got-error t)
-	  ;;   (if debug
-	  ;; 	(invoke-debugger c)
-	  ;; 	(format output "~&~a" c))
-	  ;;   *error-symbol*)
-	  )))
-    :do
-    (if (stringp pre-str)
-	(setf pre-str (concatenate 'string pre-str str +newline-string+))
-	(setf pre-str (concatenate 'string str +newline-string+)))
-    (dbugf :repl "set pre-str = ~w~%" pre-str)
-    (dbugf :repl "DO CONTIUE!!~%"))
-      result)))
+      (flet ((call-rl (prompt)
+	       (rl :eof-value 'repl-real-eof
+		   :quit-value 'repl-quit
+		   :editor editor
+		   :keymap keymap
+		   :terminal terminal
+		   :terminal-name terminal-name
+		   :terminal-class terminal-class
+		   :context :repl
+		   :prompt (or (and (stringp prompt) prompt) *default-prompt*)
+		   :output-prompt-func (and (or (symbolp prompt)
+						(functionp prompt))
+					    prompt))))
+      (loop
+	 :do
+	 (dbugf :repl "editor before = ~a~%" editor)
+	 (cond
+	   (more
+	    (setf str more)
+	    (setf more nil)
+	    (dbugf :repl "Using MORE~%"))
+	   (pre-str
+	    (setf (values str editor) (call-rl "")))
+	   (t
+	    (setf (values str editor)
+		  (call-rl (or prompt-string
+			       prompt-func
+			       'repl-output-prompt)))))
+	 (dbugf :repl "str = ~s~%editor after = ~a~%" str editor)
+	 (setf result
+	       (cond
+		 ((and (stringp str) (zerop (length str))) 'repl-empty)
+		 ((and (stringp str) (equal "." str)) 'repl-exit)
+		 ((equal str 'repl-real-eof)
+		  (dbugf :repl "You got a 'repl-real-eof~%")
+		  'repl-real-eof)
+		 ((equal str 'repl-quit) 'repl-quit)
+		 (t
+		  (dbugf :repl "Before read: pre-str = ~w str = ~w~%"
+			 pre-str str)
+		  (let ((cat (or (and pre-str (s+ pre-str str)) str)))
+		    (multiple-value-bind (obj pos)
+			(read-from-string cat nil 'repl-continue)
+		      ;; Make MORE be the rest of the string, if any.
+		      (setf more (if (and (not (eql obj 'repl-continue))
+					  (< pos (length cat)))
+				     (subseq cat pos)
+				     nil))
+		      obj)))))
+	 :while (eq result 'repl-continue)
+	 :do
+	 (setf pre-str
+	       (if (stringp pre-str)
+		   (s+ pre-str str #\newline)
+		   (s+ str #\newline)))
+	 (dbugf :repl "set pre-str = ~w~%" pre-str)
+	 (dbugf :repl "Do CONTIUE~%"))
+      result))))
 
 (defun repple-stepper (c)
   (when (find-package :deblarg)
@@ -297,89 +257,73 @@ The REPL also has a few commands:
 ;; Eval and print
 (defun repl-eval (form state)
   (with-slots (got-error error-count interceptor debug output) state
-    (cond
-      ((or (eq form *empty-symbol*) (eq form *error-symbol*))
-       ;; do nothing
-       (dbugf :repl "DO NOTHING!!~%"))
-      (t
-       (dbugf :repl "Do Something!!~%")
-       ;; If there is an interceptor, let it have a crack at it.
-       ;; If interceptor returns nil, it didn't intercept and we should go on.
-       ;; There might be some more arguments for the interceptor which it
-       ;; can read out of the more string, if it wants to.
-       (when (or (not interceptor)
-		 (and interceptor
-		      (and (not (funcall interceptor form state))
-			   (not (and (dbugf
-				      :repl
-				      "Interceptor returned NIL.~%") nil)))))
-	 (handler-case
-	     (handler-bind
-		 ;; @@@ Probably a bad idea but, ignore warnings in the REPL
-		 (
-; 		  (warning #'(lambda (c)
-; 			       (format output "~&WARNING: ~a~%" c)
-; 			       (muffle-warning)))
-; 		  #+excl (excl::compiler-note
-; 			  #'(lambda (c)
-; 			      (format output "Note: ~a~%" c)
-; 			      (continue)))
-		  #+sbcl (sb-ext::step-condition 'repple-stepper)
-		  (serious-condition
-		   #'(lambda (c)
-		       (dbugf :repl "Handler bind~%")
-		       (if debug
-			   (invoke-debugger c)
-			   (format output "Condition: ~a~%" c))))
-		  )
-	       (progn
-		 (setf - form)
-		 (dbugf :repl "About to eval ~s~%" form)
-		 (unwind-protect
-		      (let* ((vals (multiple-value-list (eval form)))
-			     (*print-circle* t))
-			;; These ***shmoozy-vars*** are in the standard,
-			;; so we'd better support them.
-			(setf /// //
-			      // /
-			      / vals
-			      *** **
-			      ** *
-			      * (car vals))
-			(loop :with len = (length vals) :and i = 0
-			   :for v :in vals
-			   :do
-			   ;; This is the "P" in REPL
-			   (format output "~&~s" v)
-			   (when (and (> len 1) (< i (- len 1)))
-			     ;; It's kind of a convention for multi-vals
-			     (format output " ;~%"))
-			   (incf i))
-			(terpri output))
-		   (setf +++ ++
-			 ++ +
-			 + -))))
-; 	     #+excl (excl::compiler-note (c)
-;                       (format output "WTF Note: ~a~%" c)
-; 		      (continue))
-	   (serious-condition
-	    (c)
-	     (dbugf :repl "Handler case, serious condition~%")
-	     (if debug
-		 (invoke-debugger c)
-		 (format output "~a~%" c)))
-	   (error (c)
-	     (dbugf :repl "Handler case, error~%")
-	     (setf got-error t)
-	     (if debug
-		 (invoke-debugger c)
-		 (format output "~a~%" c)))))))
-    (if got-error
-	(incf error-count)
-	(setf error-count 0))))
+    (when (or (eq form 'repl-empty) (eq form 'repl-error))
+      (dbugf :repl "Do Nothing~%")
+      (return-from repl-eval nil))
+    (dbugf :repl "Do Something~%")
+    ;; If there is an interceptor, let it have a crack at it.
+    ;; If interceptor returns nil, it didn't intercept and we should go on.
+    ;; There might be some more arguments for the interceptor which it
+    ;; can read out of the more string, if it wants to.
+    (when (or (not interceptor)
+	      (and interceptor
+		   (and (not (funcall interceptor form state))
+			(not (and (dbugf
+				   :repl "Interceptor returned NIL.~%") nil)))))
+      (progn
+	(setf - form)
+	(dbugf :repl "About to eval ~s~%" form)
+	(unwind-protect
+	     (let* ((vals (multiple-value-list (eval form)))
+		    (*print-circle* t))
+	       ;; These ***shmoozy-vars*** are in the standard,
+	       ;; so we'd better support them.
+	       (setf /// //
+		     // /
+		     / vals
+		     *** **
+		     ** *
+		     * (car vals))
+	       (loop :with len = (length vals) :and i = 0
+		  :for v :in vals
+		  :do
+		  ;; This is the "P" in REPL
+		  (format output "~&~s" v)
+		  (when (and (> len 1) (< i (- len 1)))
+		    ;; It's kind of a convention for values.
+		    (format output " ;~%"))
+		  (incf i))
+	       (terpri output))
+	  (setf +++ ++
+		++ +
+		+ -))))))
+
+(defmacro with-error-handling ((state) &body body)
+  `(handler-bind
+      (#+sbcl (sb-ext::step-condition 'repple-stepper)
+      (serious-condition
+       #'(lambda (c)
+	   (dbugf :repl "Handler bind~%")
+	   (setf (repl-state-got-error ,state) t)
+	   (if (repl-state-debug ,state)
+	       (invoke-debugger c)
+	       (format (repl-state-output ,state) "Condition: ~a~%" c)))))
+    ,@body))
+
+(defmacro with-repl-terminal ((terminal) &body body)
+  "If terminal isn't set Evaluate BODY with a *terminal* set up.
+TERMINAL-NAME and TERMINAL-TYPE should be in the environment."
+  (with-unique-names (thunk)
+    `(flet ((,thunk () ,@body))
+       (if ,terminal
+	   (,thunk)
+	   (with-terminal (terminal-type *terminal*
+					 :device-name terminal-name
+					 :start-at-current-line t)
+	     (,thunk))))))
 
 (defun tiny-repl (&key prompt-func prompt-string no-announce keymap
-		    terminal-name
+		    terminal terminal-name
 		    (terminal-type (pick-a-terminal-type))
 		    (output *standard-output*)
 		    (interceptor *default-interceptor*) (debug t))
@@ -389,6 +333,7 @@ to quit everything. Arguments are:
                    an instance of RL:LINE-EDITOR and a prompt string.
  PROMPT-STRING  -- 
  NO-ANNOUNCE    -- True to supress the announcement on starting.
+ TERMINAL       -- An already created terminal to use.
  TERMINAL-NAME  -- Name of a system terminal device to read from.
  TERMINAL-TYPE  -- Type of terminal to read from. Defaults from
                    pick-a-terminal-type and so from *default-terminal-type*.
@@ -413,6 +358,7 @@ to quit everything. Arguments are:
 		:prompt-func    prompt-func
 		:prompt-string  prompt-string
 		:keymap         keymap
+		:terminal       terminal
 		:terminal-class (find-terminal-class-for-type terminal-type)
 		:output         output))
 	(result nil)
@@ -423,9 +369,7 @@ to quit everything. Arguments are:
     (when (not theme:*theme*)
       (setf theme:*theme* (theme:default-theme)))
 
-    (with-terminal (terminal-type *terminal*
-				  :device-name terminal-name
-				  :start-at-current-line t)
+    (with-repl-terminal (terminal)
       (setf (tt-input-mode) :line)
       ;; Activate the debugger if it's loaded.
       (when (and debug (find-package :deblarg))
@@ -434,20 +378,21 @@ to quit everything. Arguments are:
 	   (tagbody
 	    TOP
 	      (restart-case
-		  (loop :do
-		     (setf (repl-state-got-error state) nil)
-		     (setf result (repl-read state)) ;;; READ
-		     :until (or (equal result *real-eof-symbol*)
-				(equal result *exit-symbol*))
-		     :do
-		     (dbugf :repl "~s (~a) ~s~%"
-			    result (type-of result)
-			    (eq result *empty-symbol*))
-		     (if (equal result *quit-symbol*)
-			 (when (confirm-quit state *repl-level*)
-			   (return result))
-			 (repl-eval result state)) ;;; EVAL
-		     :finally (return result))
+		  (with-error-handling (state)
+		    (loop :do
+		       (setf (repl-state-got-error state) nil)
+		       (setf result (repl-read state)) ;;; READ
+		       :until (or (equal result 'repl-real-eof)
+				  (equal result 'repl-exit))
+		       :do
+		       (dbugf :repl "~s (~a) ~s~%"
+			      result (type-of result)
+			      (eq result 'repl-empty))
+		       (if (equal result 'repl-quit)
+			   (when (confirm-quit state *repl-level*)
+			     (return result))
+			   (repl-eval result state)) ;;; EVAL
+		       :finally (return result)))
 		(abort ()
 		  :report
 		  (lambda (stream)
@@ -465,21 +410,12 @@ to quit everything. Arguments are:
 	(when (> (repl-state-error-count state) 8)
 	  (format output "Quit due to too many errors.~%"))
 
-	(setq want-to-quit
+	(setf want-to-quit
 	      (cond
-		((eq result *real-eof-symbol*)
-		 (format output "*EOF*~%")
-		 t)
-		((eq result *quit-symbol*)
-		 (format output "*Quit*~%")
-		 t)
-		((eq result *exit-symbol*)
-		 (format output "*Exit*~%")
-		 nil)
-		(t
-		 (dbugf :repl "~s ~a~%" result (type-of result))
-		 t)))
-
+		((eq result 'repl-real-eof) (format output "*EOF*~%") t)
+		((eq result 'repl-quit) (format output "*Quit*~%")    t)
+		((eq result 'repl-exit) (format output "*Exit*~%")    nil)
+		(t (dbugf :repl "~s ~a~%" result (type-of result))    t)))
 	(setf *debugger-hook* old-debugger-hook)
 	(decf *repl-level*))
       want-to-quit)))
