2b638efa327adb983795481d6e3cffa594001c38
Nibby Nebbulous <nibbula@gmail.com>
2019-01-02 08:28:54 -0800

    fix for uname on openbsd

--- unix/environmental.lisp
+++ unix/environmental.lisp
@@ -1234,7 +1234,7 @@ Returns an integer."
   (machine    :char :count #.+UTSNAME-LENGTH+)
   (domainname :char :count #.+UTSNAME-LENGTH+))
 
-#+freebsd
+#+(or freebsd openbsd)
 (defcstruct utsname
   (sysname    :char :count #.+UTSNAME-LENGTH+)
   (nodename   :char :count #.+UTSNAME-LENGTH+)
