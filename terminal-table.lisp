0a0f0d53e95886511683365d48206c32abd2059d
Nibby Nebbulous <nibbula@gmail.com>
2019-02-19 03:42:16 -0800

    get rid of useless duplication

--- terminal-table.lisp
+++ terminal-table.lisp
@@ -74,88 +74,11 @@
 	(when (or (not *max-width*) (< cursor (1- *max-width*)))
 	  (terminal-write-char stream #\newline))))))
 
-(defmethod text-table-adjust-sizes (table (renderer terminal-table-renderer)
-				    sizes max-width)
-  "Adjust the sizes in the vector SIZES, by the data in TABLE, limited
-to MAX-WIDTH.."
-  (let ((sep-len (length (text-table-renderer-separator renderer))))
-    (omap
-     #'(lambda (row)
-	 (let ((len (olength row)) (i 0) (new-size nil) (col 0) size)
-	   (omap
-	    #'(lambda (field)
-		(setf size (aref sizes i)
-		      new-size
-		      (if (and size (minusp size))
-			  size		; Don't mess with preset size
-			  (max (or size 0)
-			       (typecase field
-				 ((or string fat-string)
-				  (display-length field))
-				 (otherwise
-				  (display-length
-				   (princ-to-string field)))))))
-		;;(incf col (abs new-size))
-		(when (and max-width (> (+ col (abs new-size)) max-width))
-		  (setf new-size (max 0
-				      (- new-size
-					 (- (+ col (abs new-size))
-					    max-width)))))
-		(incf col (abs new-size))
-		(when (< i (1- len))
-		  (incf col sep-len))
-		(setf (aref sizes i) new-size)
-		(incf i))
-	    row)))
-     table)))
-
 (defmethod text-table-cell-lines (table (renderer terminal-table-renderer)
 				  cell width)
   "Return the lines of CELL fitting in WIDTH."
   (declare (ignore table renderer))
   (osplit #\newline (with-output-to-fat-string (str)
 		      (justify-text cell :cols width :stream str))))
-
-(defun %output-cell (renderer table cell width justification row column)
-  (declare (ignore row))
-  (with-slots ((cursor table-print::cursor)) renderer
-    (let* ((op (typecase cell
-		 ((or string fat-string) "/fatchar-io:print-string/")
-		 (t "a")))
-	   (fmt (cond
-		  ((and (= column (1- (olength (table-columns table))))
-			(not *trailing-spaces*))
-		   (s+ "~*~" op))
-		  ((eql justification :right)
-		   (s+ "~v@" op))
-		  ((and (not justification) (typep cell 'number))
-		   (s+ "~v@" op))
-		  (t
-		   (s+ "~v" op))))
-	   (field (let ((*print-pretty* nil))
-		    (with-output-to-fat-string (str)
-		      (format str fmt width cell))))
-	   (len (display-length field)))
-      (if (and (eq justification :overflow)
-	       (> len width))
-	  (progn
-	    (write field :stream *destination* :escape nil :readably nil
-		   :pretty nil)
-	    (format *destination* "~%~v,,,va" width #\space #\space)
-	    (setf cursor width))
-	  (typecase cell
-	    (standard-object
-	     (princ (osubseq field 0 (min width (olength field)))
-		    *destination*)
-	     (incf cursor (min width (olength field))))
-	    (t
-	     (write (osubseq field 0 (min width (olength field)))
-		    :stream *destination* :escape nil :readably nil :pretty nil)
-	     (incf cursor (min width (olength field)))))))))
-
-(defmethod table-output-cell ((renderer terminal-table-renderer)
-			      table cell width justification row column)
-  "Output a table cell."
-  (%output-cell renderer table cell width justification row column))
 
 ;; EOF
