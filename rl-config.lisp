2f07d7186f24e5977ccf9b7b6238d27ed5dd0294
Nibby Nebbulous <nibbula@gmail.com>
2019-05-17 13:19:40 -0700

    initial

---
+++ rl-config.lisp
@@ -0,0 +1,28 @@
+;;
+;; rl-config.lisp - Configuration options for RL.
+;;
+
+(defpackage :rl-config
+  (:documentation "Configuration options for RL.")
+  (:use :cl :config)
+  (:export))
+(in-package :rl-config)
+
+;; (declaim (optimize (debug 2)))
+(declaim (optimize (speed 0) (safety 3) (debug 3) (space 0) (compilation-speed 0)))
+
+(defvar *configuration* nil)
+(setf *configuration* nil)
+
+(defconfig use-sqlite boolean-feature
+   "True to use CLSQL-SQLITE for command history storage."
+   (library-loadable-p '((t "libsqlite3.so"))))
+
+;; (defconfiguration
+;;   ((use-sqlite boolean-feature
+;;     "True to use CLSQL-SQLITE for command history storage."
+;;     (library-loadable-p '((t "libsqlite3.so"))))))
+
+(configure)
+
+;; EOF
