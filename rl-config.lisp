601dbcaaeb66375fd3017801e0ab2f26b383d4ba
Nibby Nebbulous <nibbula@gmail.com>
2019-05-19 06:52:46 -0700

    use defconfiguration instead of defconfig

--- rl-config.lisp
+++ rl-config.lisp
@@ -11,18 +11,23 @@
 ;; (declaim (optimize (debug 2)))
 (declaim (optimize (speed 0) (safety 3) (debug 3) (space 0) (compilation-speed 0)))
 
-(defvar *configuration* nil)
-(setf *configuration* nil)
+;; (defvar *configuration* nil)
+;; (setf *configuration* nil)
 
-(defconfig use-sqlite boolean-feature
-   "True to use CLSQL-SQLITE for command history storage."
-   (library-loadable-p '((t "libsqlite3.so"))))
+;; (defconfig use-sqlite boolean-feature
+;;    "True to use CLSQL-SQLITE for command history storage."
+;;    (library-loadable-p '((t "libsqlite3.so"))))
 
-;; (defconfiguration
-;;   ((use-sqlite boolean-feature
-;;     "True to use CLSQL-SQLITE for command history storage."
-;;     (library-loadable-p '((t "libsqlite3.so"))))))
+(defconfiguration
+  ((use-sqlite boolean-feature
+    "True to use CLSQL-SQLITE for command history storage."
+    (library-loadable-p '((t "libsqlite3.so"))))))
 
+;;(configure :verbose t)
 (configure)
+
+;; (let ((*print-length* nil))
+;;   (format t "~s~%~%*configuration* ~s~%~%*config* ~s~%"
+;; 	  *features* *configuration* *config*))
 
 ;; EOF
