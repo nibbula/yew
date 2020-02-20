fcd63fb42bd265557d569469350d917365d21fec
Nibby Nebbulous <nibbula@gmail.com>
2020-02-20 00:46:44 -0800

    get rid of the optimize thing

--- view-image-x11.lisp
+++ view-image-x11.lisp
@@ -19,7 +19,7 @@
 ;; (declaim (optimize (speed 0) (safety 3) (debug 3) (space 0) (compilation-speed 0)))
 ;;(declaim (optimize (speed 3) (safety 0) (debug 2) (space 0) (compilation-speed 0)))
 
-(declaim #.`(optimize ,.view-image::*optimize-settings*))
+;;(declaim #.`(optimize ,.view-image::*optimize-settings*))
 
 (defclass image-x11-inator (image-inator)
   ((display
