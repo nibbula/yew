87bb5553e137ac023f3312efbc003bdf24532b14
Nibby Nebbulous <nibbula@gmail.com>
2020-03-13 02:02:10 -0700

    adjust settings
    i'm not even sure this is a good idea anymore

--- los-config.lisp
+++ los-config.lisp
@@ -16,9 +16,10 @@
     "Default optimization settings for each file/compilation unit?."
     ;; If we don't have at least debug 2, then most compilers won't save
     ;; the function arguments.
-    ;; `((debug 2)))))
-    '((speed 0) (safety 3) (debug 3) (space 0) (compilation-speed 0)))))
-    ;; '((speed 3) (safety 0) (debug 2) (space 0) (compilation-speed 0)))))
+    `((debug 2))
+    ;; '((speed 0) (safety 3) (debug 3) (space 0) (compilation-speed 0))
+    ;; '((speed 3) (safety 0) (debug 2) (space 0) (compilation-speed 0))
+    )))
 
 (configure)
 
