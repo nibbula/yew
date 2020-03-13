f399543ee8d8894df14a3817c1db627e4aa33016
Nibby Nebbulous <nibbula@gmail.com>
2020-03-13 02:05:05 -0700

    adjust settings
    this probably isn't even a good idea

--- terminal-config.lisp
+++ terminal-config.lisp
@@ -16,7 +16,10 @@
     "Default optimization settings for each file/compilation unit?."
     ;; If we don't have at least debug 2, then most compilers won't save
     ;; the function arguments.
-    `((debug 2)))))
+    `((debug 2))
+    ;; `((safety 3) (debug 3)))))
+    ;; `((speed 0) (safety 3) (debug 3) (space 0) (compilation-speed 0))
+    )))
 
 (configure)
 
