437440db9ebe8626d65bcb0658d13a943e6d0f36
Nibby Nebbulous <nibbula@gmail.com>
2020-03-24 06:31:57 -0700

    add some documentation and a print-theme command

--- theme.lisp
+++ theme.lisp
@@ -1,9 +1,94 @@
-;;
-;; theme.lisp - Customize the style.
-;;
+;;;
+;;; theme.lisp - Customize the style.
+;;;
 
 (defpackage :theme
-  (:documentation "Customize the style.")
+  (:documentation "Appearance customization.
+
+This is for customizable appearance that affects many programs or applications.
+Things should probably only be added here if they are applicable to multiple
+programs. This probably should not be used for customization that affects
+behavior, since that is more appropriately done by a system or package that is
+responsible for that behavior, since the specifics of such settings may vary
+with the version of that code.
+
+This enables the user to able to confidently change the look of things without
+changing the functionality.
+
+If one desires to have different theme settings for different programs, one can
+invoke that program with a special dynamic setting of *theme*.
+
+HOW TO USE:
+
+  The current theme is stored in the *THEME* variable. An easy way to see
+  what's in the theme is with PRINT-THEME, e.g.:
+    (print-theme *theme*).
+
+  THEME-VALUE is an accessor for things in the theme. You can a get a specific
+  value like:
+    (theme-value *theme* '(:command :not-found :style))
+  or set it like:
+    (setf (theme-value *theme* '(:command :not-found :style)) '(:yellow))
+
+  The theme is like a tree, which divides the theme into categories and
+  sub-categories. Branch and value names in the theme are usually keywords, to
+  avoid symbol package issues. Values can be anything, but a common type of
+  value is a :STYLE value, which is currently mostly text styles, and mostly
+  in the vocabulary of terminal attributes as described in
+  fatchar:span-to-fatchar-string.
+
+  The default theme is returned by DEFAULT-THEME, so you can always set *THEME*
+  back to that, e.g. (setf *theme* (default-theme))
+
+  You can save a theme to a file with SAVE-THEME, and load it in back in with
+  LOAD-THEME.
+
+  If you'd like the theme as tree, you can use THEME-AS-TREE. For example, to
+  view it in the tree viewer, you could say:
+   (tree-viewer:view-tree (theme:theme-as-tree theme:*theme*))
+  You might also want it as a linear list of values, with the full paths,
+  which you can get with THEME-LIST. Note that both getting the theme as a
+  tree and a list, loses the descriptions, so shouldn't be considered as
+  equivalent to the theme.
+
+  You can convert your old DIRCOLORS from GNU 'ls' with DIRCOLORS-TO-THEME, e.g.
+  (set-theme-items *theme* (dircolor-to-theme)) would set values in the current
+  theme to things from the LS_COLORS environment variable.
+
+  If you're stuck in an output device with only 16 colors, you can get the 16
+  color version of the default theme with (default-theme-16-color), or just set
+  it in the current theme with (set-theme-defaults-for-16-color *theme*).
+
+USE FROM PROGRAMS:
+
+  Programs will mostly use THEME-VALUE, to get a value for a specific setting.
+  Programs should probably not change anything in the theme unless the user
+  requests it.
+
+CREATING AND CUSTOMIZING THEMES:
+
+  Probably the easiest way to customize a theme, is using set-theme-values in
+  a startup file. For example:
+    (theme:set-theme-items theme:*theme*
+      '((:program :empty-line-indicator :character) #\~
+        (:program :modeline :style)                 (:bg-blue :fg-cyan)
+        (:program :search-match :style)             (:red :underline)))
+
+  Another way would be customize the current theme with THEME-VALUE, e.g.:
+    (setf (theme-value *theme* '(:program :modeline :style))
+                               '(:fg :color #(:rgb 0 .80 1)
+                                 :bg :color #(:rgb .4 0 .5)))
+  and then save it to a file with SAVE-THEME, e.g.:
+    (save-theme *theme* (opsys:path-append (opsys:data-dir \"lisp\") \"theme\"))
+  and load it in with LOAD-THEME from your startup file, e.g.:
+    (setf *theme* (load-theme
+                    (opsys:path-append (opsys:data-dir \"lisp\") \"theme\")))
+
+  To create a whole theme from scratch, one could do it with make-theme and
+  set-theme-values, like default-theme does. Or you could save the default theme
+  to a file and edit it.
+
+  In the future there should be theme creation tool with a nice user interface.")
   (:use :cl :dlib :fatchar)
   (:export
    #:theme
@@ -359,6 +444,18 @@ this loses information, such as descriptions and titles."
 	  (print-theme-node n :stream stream :max-size max-size)))))
   (values))
 
+#+lish
+(lish:defcommand print-theme
+  ((theme object :default '*theme* :help "The theme to print.")
+   (as-tree boolean :short-arg #\t :help "Print a a tree.")
+   (readably boolean :short-arg #\r
+    :help "Print as a Lisp object that can read by READ."))
+  "Print a description of a theme."
+  (when (not theme)
+    (setf theme (or lish:*input* *theme*)))
+  (print-theme theme :as-tree as-tree :readably readably)
+  (setf lish:*output* theme))
+
 (defun make-theme (name &rest keys &key title description)
   "Make a new theme named NAME. If TITLE isn't given it's set from name."
   (declare (ignorable title description))
@@ -576,9 +673,6 @@ Something like the default setting of typical GNU tools."))))
        (:program :selection            :style)     (:standout)
        (:program :label		       :style)     (:cyan)
        (:program :data		       :style)     (:green)
-       ;; Line editor
-       ;; (:rl      :search-match         :style)     (:underline :red)
-       ;; (:rl      :selection            :style)     (:standout)
        ))
     tt))
 
