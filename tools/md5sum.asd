aa20dcba78384ec017faae5d75c3017f73973220
Nibby Nebbulous <nibbula@gmail.com>
2021-05-12 05:40:22 -0700

    initial

---
+++ tools/md5sum.asd
@@ -0,0 +1,15 @@
+;;;								-*- Lisp -*-
+;;; md5sum.asd - System definition for md5sum
+;;;
+
+(defsystem md5sum
+    :name               "md5sum"
+    :description        "MD5 message digest checksums."
+    :version            "0.1.0"
+    :author             "Nibby Nebbulous <nibbula -(. @ .)- uucp!gmail.com>"
+    :license            "GPLv3"
+    :source-control	:git
+    :long-description   "MD5 message digest checksums."
+    :depends-on (:dlib :opsys :md5)
+    :components
+    ((:file "md5sum")))
---
+++ tools/md5sum.lisp
@@ -0,0 +1,52 @@
+;;;
+;;; md5sum.lisp - MD5 message digest checksums.
+;;;
+
+(defpackage :md5sum
+  (:documentation "MD5 message digest checksums.")
+  (:use :cl :dlib :collections)
+  (:export
+   #:md5sum
+   #:!md5sum
+   ))
+(in-package :md5sum)
+
+(defun md5sum (files &key collect as-bytes)
+  (flet ((sum-thing (thing)
+	   (typecase thing
+	     (pathname (md5:md5sum-file thing))
+	     (stream (md5:md5sum-stream thing))
+	     (string
+	      (if (nos:file-exists thing)
+		  (md5:md5sum-file thing)
+		  (md5:md5sum-string thing)))
+	     (sequence
+	      (md5:md5sum-sequence thing))
+	     (t
+	      (error "Don't know how to MD5sum a ~s." (type-of thing))))))
+  (grout:with-grout ()
+    (let (results)
+      (loop :with bytes :and sum
+	 :for f :in files
+	 :do
+	 (with-simple-restart (continue "Skip to the next thing to checkum.")
+	   (setf bytes (sum-thing f)
+		 sum (with-output-to-string (str)
+		       (map nil #'(lambda (x)
+				    (format str "~(~2,'0x~)" x))
+			    bytes)))
+	   (grout:grout-format "~a  ~a~%" sum f)
+	   (when collect
+	     (push (if as-bytes bytes sum) results))))
+      (when collect
+	(nreverse results))))))
+
+#+lish
+(lish:defcommand md5sum
+  ((collect boolean :short-arg #\c :help "Collect results in a list.")
+   (as-bytes boolean :short-arg #\s :help "Return results as byte arrays.")
+   (files pathname :repeating t :help "Files to sum."))
+  "Print MD5 checksums. Use a stronger checksum for anything important."
+  (md5sum (or files (list lish:*input*)) :collect collect :as-bytes as-bytes))
+
+;; End
