dbc399a2e4a11e3a7b7793b21758303df23caa46
Nibby Nebbulous <nibbula@gmail.com>
2021-05-15 17:59:42 -0700

    maybe fix it to work better with binary streams?
    needs testing

--- los/tee.lisp
+++ los/tee.lisp
@@ -11,18 +11,36 @@
    ))
 (in-package :tee)
 
+(defun is-binary (stream)
+  "Return true if ‘stream’ has an element-type of (unsigned-byte 8)."
+  (equal (stream-element-type stream) '(unsigned-byte 8)))
+
+(defun all-outputs-are-binary (files-or-streams)
+  "Return true if all elements of the list ‘files-or-streams’ are strings or
+pathnames, or satisfy ‘is-binary.’."
+  (every (_ (or (pathnamep _) (stringp _) (and (streamp _) (is-binary _))))
+	 files-or-streams))
+
 (defun tee (&rest files)
   (let* (to-close
 	 (source *standard-input*)
+	 (all-binary-out (all-outputs-are-binary (append (list source) files)))
+	 ;; @@@ The default is 'character not :default, so we shouldn't use
+	 ;; :default here right?
+	 (output-element-type (if all-binary-out '(unsigned-byte 8) 'character))
 	 (input
-	   (if (equal (stream-element-type source) '(unsigned-byte 8))
+	   ;; If the source in binary, and all the outputs aren't, we convert to
+	   ;; utf8b.
+	   (if (and (is-binary source) (not all-binary-out))
 	       (make-instance 'utf8b-input-stream :input-stream source)
 	       source))
 	 (buffer-size dlib:*buffer-size*)
 	 (outputs)
 	 (buf (make-array buffer-size
-			  :element-type (stream-element-type source)))
+			  :element-type (stream-element-type input)))
 	 (pos 0))
+    ;; (format *debug-io* "all-binary-out ~s~%output-element-type ~s~%"
+    ;; 	    all-binary-out output-element-type)
     (unwind-protect
 	 (progn
 	   (setf outputs
@@ -39,9 +57,13 @@
 				      (if (uos:isatty f)
 					  (open f :direction :output
 						  :if-does-not-exist :create
-						  :if-exists :append)
+						  :if-exists :append
+						  :element-type
+						  output-element-type)
 					  (open f :direction :output
-						  :if-does-not-exist :create)))
+						  :if-does-not-exist :create
+						  :element-type
+						  output-element-type)))
 				(push stream to-close)
 			     :and
 			       :collect stream
