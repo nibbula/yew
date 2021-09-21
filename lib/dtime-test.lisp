42611dd41c8d83dec6f34b2e512e0867a17fb69a
Nibby Nebbulous <nibbula@gmail.com>
2021-09-21 15:01:40 -0700

    test for describe-duration

--- lib/dtime-test.lisp
+++ lib/dtime-test.lisp
@@ -71,8 +71,54 @@
 	       (dtime-round t2 :microseconds))))
   )
 
+(deftests (dtime-2 :doc "Test more stuff.")
+  "Test describe-duration."
+  (equal (describe-duration (make-dtime :seconds 1)) "1 second")
+  (equal (describe-duration (make-dtime :seconds 2)) "2 seconds")
+  (equal (describe-duration (make-dtime :seconds 60)) "1 minute")
+  (equal (describe-duration (make-dtime :seconds 120)) "2 minutes")
+  (equal (describe-duration (make-dtime :seconds 119))
+	 "1 minute 59 seconds")
+  (equal (dtime::describe-duration
+	 (make-dtime
+	  :seconds (+ 1 60
+                      (* 60 60)
+                      (* 24 60 60)
+                      (* 7 24 60 60)
+                      (* (+ 365 1/4) 24 60 60)
+                      (* 10 (+ 365 1/4) 24 60 60)
+                      (* 100 (+ 365 1/4) 24 60 60)
+                      (* 1000 (+ 365 1/4) 24 60 60))))
+  "1 millennium 1 century 1 decade 1 year 1 week 1 day 1 hour 1 minute 1 second")
+  (equal
+   (describe-duration (make-dtime :seconds 1 :nanoseconds 1))
+   "1 second 1 nanosecond")
+  (equal
+   (describe-duration (make-dtime :seconds 2 :nanoseconds 2))
+   "2 seconds 2 nanoseconds")
+  (equal
+   (describe-duration (make-dtime :seconds 2 :nanoseconds 20002))
+   "2 seconds 20 microseconds 2 nanoseconds")
+  (equal
+   (describe-duration (make-dtime :seconds 2 :nanoseconds 20002002))
+   "2 seconds 2 centiseconds 2 microseconds 2 nanoseconds")
+  (equal
+   (describe-duration (make-dtime :seconds 2 :nanoseconds 2002020020))
+   "4 seconds 2 milliseconds 20 microseconds 20 nanoseconds")
+  (equal
+   (describe-duration (make-dtime :seconds 2 :nanoseconds 33000000000001))
+   "9 hours 10 minutes 2 seconds 1 nanosecond")
+  (equal
+   (describe-duration (make-dtime :seconds 1 :nanoseconds 200030100))
+   "1 second 2 deciseconds 30 microseconds 100 nanoseconds")
+  (equal
+   (describe-duration (make-dtime :seconds 12392835611))
+   "3 centuries 9 decades 2 years 36 weeks 5 days 14 hours 20 minutes 11 seconds")
+  )
+
 (deftests (dtime-all :doc "Test :dtime.")
-  dtime-1)
+  dtime-1
+  dtime-2)
 
 (defun run ()
   (run-group-name 'dtime-all :verbose t))
