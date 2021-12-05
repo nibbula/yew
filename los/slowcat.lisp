5374bd6c88069c4e499f274ec9b1b756ae2b866f
Nibby Nebbulous <nibbula@gmail.com>
2021-12-05 04:09:24 -0800

    make it work again
    and get rid of some dead code

--- los/slowcat.lisp
+++ los/slowcat.lisp
@@ -8,7 +8,6 @@
   (:export
    ;; Main entry point
    #:slowcat
-   ;#:slowcat-main
    #:!slowcat
    ))
 (in-package :slowcat)
@@ -16,17 +15,7 @@
 (declaim #.`(optimize ,.(getf los-config::*config* :optimization-settings)))
 
 (defun slowcat (stream-or-file &key (delay .02) (unit :char))
-  ;; (flet ((catty (s)
-  ;; 	   (loop :with line
-  ;; 	      :while (setf line (read-line s nil))
-  ;; 	      :do
-  ;; 	      (write-line line)
-  ;; 	      (sleep delay))))
-  ;;   (if (streamp stream-or-file)
-  ;; 	(catty stream-or-file)
-  ;; 	(with-open-file (s (quote-filename stream-or-file) :direction :input)
-  ;; 	  (catty s)))))
-  (with-open-file-or-stream (s (quote-filename stream-or-file) :direction :input)
+  (with-open-file-or-stream (s stream-or-file :direction :input)
     (ecase unit
       (:line
        (loop :with line
@@ -43,35 +32,19 @@
 	  (finish-output)
 	  (sleep delay))))))
 
-#| This seems fairly superfluous with lish working
-
-(defun slowcat-main ()
-  "Invocation of slowcat from the command line."
-  (if (<= (length (nos:lisp-args)) 2)
-      (slowcat-stream *standard-input*)
-      (let* ((args (nos:lisp-args))
-	     (delay .02)
-	     (p (position "-d" (nos:lisp-args) :test #'string=))
-	     (d (and p (read-from-string (elt (nos:lisp-args) (1+ p))))))
-	(when (and d (numberp d))
-	  (setf delay d)
-	  (setf args (loop :for i :from 0 :below (length args)
-			:if (not (or (= i p) (= i (1+ p))))
-			:collect (elt args i))))
-	(loop :for file :in (subseq args 2)
-	   :do
-	   (slowcat file :delay delay)))))
-|#
-
 #+lish
 (lish:defcommand slowcat
-  ((delay number :short-arg #\d :default .001)
-   (unit choice :short-arg #\u :default :char :choices '(:line :char))
-   (files pathname :repeating t))
+  ((delay number :short-arg #\d :default .001 :help "Delay in seconds.")
+   (unit choice :short-arg #\u :default :char :choices '(:line :char)
+    :help "Whether ‘dalay’ applies to characters or lines.")
+   (files pathname :repeating t
+    :help "Files or stream-like things to use as input."))
   "Make things go by slowly, like the old days."
   (if files
       (loop :for f :in files :do
-	 (slowcat f :delay delay :unit unit))
-      (slowcat *standard-input* :delay delay :unit unit)))
+	(lish:with-streamlike-input (f)
+	  (slowcat f :delay delay :unit unit)))
+      (progn
+	(slowcat *standard-input* :delay delay :unit unit))))
 
-;; EOF
+;; End
