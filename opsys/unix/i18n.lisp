bad5936859054a982a77e0e762edf683a5bbf8fc
Nibby Nebbulous <nibbula@gmail.com>
2023-05-05 01:26:46 -0700

    fixes for darwin

--- opsys/unix/i18n.lisp
+++ opsys/unix/i18n.lisp
@@ -7,7 +7,7 @@
 (declaim #.`(optimize ,.(getf opsys-config::*config* :optimization-settings)))
 
 (defun language ()
-  "Return a the system language."
+  "Return the system language."
   (let (lang)
     ;; This is lame. I'd like to turn these into something more readable
     ;; to humans, but that seems likely to bitrot and error.
@@ -43,6 +43,7 @@
   "Natural language items available from nl-langinfo.")
 
 ;; The order here is important and for GNU libc on linux.
+#+linux
 (define-enum-list *nl-items*
   #(#(+ABDAY-1+     "Abbreviated name of the first day of the week.")
     #(+ABDAY-2+     "Abbreviated name of the second day of the week.")
@@ -103,6 +104,7 @@
     )
   :start #.(nl-item-value +LC-TIME+ 0))
 
+#+linux
 (define-enum-list *nl-items*
   #(#(+DECIMAL-POINT+            "Decimal point character.")
     #(+RADIXCHAR+                "Radix character.")
@@ -114,6 +116,7 @@
     )
   :start #.(nl-item-value +LC-NUMERIC+ 0))
   
+#+linux
 (define-enum-list *nl-items*
   #(#(+YESEXPR+     "Affirmative response expression.")
     #(+NOEXPR+      "Negative response expression.")
@@ -121,6 +124,7 @@
     #(+NOSTR+       "Output string for ‘no’."))
   :start #.(nl-item-value +LC-MESSAGES+ 0))
 
+#+linux
 (define-enum-list *nl-items*
   #(#(+CRNCYSTR+
     "Local currency symbol, preceded by '-' if the symbol should appear
@@ -128,6 +132,82 @@ before the value, '+' if the symbol should appear after the value, or '.'
 if the symbol should replace the radix character. If the local currency symbol
 is the empty string, implementations may return the empty string ("")."))
   :start #.(nl-item-value +LC-MONETARY+ 0))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; darwin
+
+;; order is important
+#+darwin
+(define-enum-list *nl-items*
+  #(#(+D-T-FMT+     "String for formatting date and time.")
+    #(+D-FMT+       "Date format string.")
+    #(+T-FMT+       "Time format string.")
+    #(+T-FMT-AMPM+  "a.m. or p.m. time format string.")
+    #(+AM-STR+      "Ante-meridiem affix.")
+    #(+PM-STR+      "Post-meridiem affix.")
+
+    #(+DAY-1+       "Name of the first day of the week (for example, Sunday).")
+    #(+DAY-2+       "Name of the second day of the week (for example, Monday).")
+    #(+DAY-3+       "Name of the third day of the week (for example, Tuesday).")
+    #(+DAY-4+       "Name of the fourth day of the week (for example, Wednesday).")
+    #(+DAY-5+       "Name of the fifth day of the week (for example, Thursday).")
+    #(+DAY-6+       "Name of the sixth day of the week (for example, Friday).")
+    #(+DAY-7+       "Name of the seventh day of the week example, Saturday).")
+
+    #(+ABDAY-1+     "Abbreviated name of the first day of the week.")
+    #(+ABDAY-2+     "Abbreviated name of the second day of the week.")
+    #(+ABDAY-3+     "Abbreviated name of the third day of the week.")
+    #(+ABDAY-4+     "Abbreviated name of the fourth day of the week.")
+    #(+ABDAY-5+     "Abbreviated name of the fifth day of the week.")
+    #(+ABDAY-6+     "Abbreviated name of the sixth day of the week.")
+    #(+ABDAY-7+     "Abbreviated name of the seventh day of the week.")
+
+    #(+MON-1+       "Name of the first month of the year.")
+    #(+MON-2+       "Name of the second month.")
+    #(+MON-3+       "Name of the third month.")
+    #(+MON-4+       "Name of the fourth month.")
+    #(+MON-5+       "Name of the fifth month.")
+    #(+MON-6+       "Name of the sixth month.")
+    #(+MON-7+       "Name of the seventh month.")
+    #(+MON-8+       "Name of the eighth month.")
+    #(+MON-9+       "Name of the ninth month.")
+    #(+MON-10+      "Name of the tenth month.")
+    #(+MON-11+      "Name of the eleventh month.")
+    #(+MON-12+      "Name of the twelfth month.")
+
+    #(+ABMON-1+     "Abbreviated name of the first month.")
+    #(+ABMON-2+     "Abbreviated name of the second month.")
+    #(+ABMON-3+     "Abbreviated name of the third month.")
+    #(+ABMON-4+     "Abbreviated name of the fourth month.")
+    #(+ABMON-5+     "Abbreviated name of the fifth month.")
+    #(+ABMON-6+     "Abbreviated name of the sixth month.")
+    #(+ABMON-7+     "Abbreviated name of the seventh month.")
+    #(+ABMON-8+     "Abbreviated name of the eighth month.")
+    #(+ABMON-9+     "Abbreviated name of the ninth month.")
+    #(+ABMON-10+    "Abbreviated name of the tenth month.")
+    #(+ABMON-11+    "Abbreviated name of the eleventh month.")
+    #(+ABMON-12+    "Abbreviated name of the twelfth month.")
+
+    #(+ERA+         "Era description segments.")
+    #(+ERA-D-FMT+   "Era date format string.")
+    #(+ERA-D-T-FMT+ "Era date and time format string.")
+    #(+ERA-T-FMT+   "Era time format string.")
+    #(+ALT-DIGITS+  "Alternative symbols for digits.")
+
+    #(+RADIXCHAR+   "Radix character.")
+    #(+THOUSEP+     "Separator for thousands.")
+
+    #(+YESEXPR+     "Affirmative response expression.")
+    #(+NOEXPR+      "Negative response expression.")
+    #(+YESSTR+      "Output string for ‘yes’.")
+    #(+NOSTR+       "Output string for ‘no’.")
+    #(+CRNCYSTR+
+    "Local currency symbol, preceded by '-' if the symbol should appear
+before the value, '+' if the symbol should appear after the value, or '.'
+if the symbol should replace the radix character. If the local currency symbol
+is the empty string, implementations may return the empty string ("").")
+    )
+  :start 1)
 
 ;; We treat locale like an opaque pointer, even though it's not.
 (defctype locale-t :pointer
