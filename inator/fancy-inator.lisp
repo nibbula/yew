91246c6deba2b9a0f6678bc51855695561504624
Nibby Nebbulous <nibbula@gmail.com>
2023-12-27 17:54:32 -0800

    make fancy-inator be a terminal-inator and set it's keymap

--- inator/fancy-inator.lisp
+++ inator/fancy-inator.lisp
@@ -4,7 +4,7 @@
 
 (defpackage :fancy-inator
   (:documentation "A terminal-inator with fancy features.")
-  (:use :cl :inator :char-util :keymap :fatchar :terminal)
+  (:use :cl :inator :char-util :keymap :fatchar :terminal :terminal-inator)
   (:export
    ;; Classes
    #:fancy-inator
@@ -20,17 +20,23 @@
 ;; Right now the fancy features are just eval-expression.
 ;; :terminal-inator :fui :tiny-repl
 
-(defclass fancy-inator (inator)
-  ()
-  (:documentation "An inator with fancy features."))
-
 (defkeymap *default-fancy-inator-escape-keymap* ()
+  `((#\escape . eval-expression-command)))
+
+(defkeymap *default-fancy-inator-keymap* ()
   `((#\escape . *default-fancy-inator-escape-keymap*)))
+
+(defclass fancy-inator (terminal-inator)
+  ()
+  (:default-initargs
+   :default-keymap *default-fancy-inator-keymap*)
+  (:documentation "An inator with fancy features."))
 
 (defgeneric eval-expression-command (inator)
   (:documentation "Prompt for an expression an evaluate it.")
   (:method ((o inator))
     (tt-move-to (1- (tt-height)) 0)
+    (tt-finish-output)
     (with-simple-restart (abort "Go back to the inator.")
       (handler-case
 	  (fui:display-text
@@ -40,11 +46,10 @@
 		       (let ((*standard-output* stream))
 			 (tiny-repl:tiny-repl :prompt-string "Eval: "
 					      :quietly t :once t
-					      :output stream))
+					      :output stream
+					      :terminal *terminal*))
 		     (redraw o)
-		     (update-display o)
-		     ;; (setf (tt-input-mode) :char)
-		     )))
+		     (update-display o))))
 	   :justify nil :min-width 16)
 	(error (c)
 	  (if (fui:popup-y-or-n-p
